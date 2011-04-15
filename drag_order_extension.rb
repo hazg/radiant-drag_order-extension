e7c37365ee92a55a503346fbb9ae20d44da0c2be
Gerrit Kaiser <gerrit@gerritkaiser.de>
2011-04-15 19:19:09 +0100

    move routes to extension file to work around route priority issues in edge radiant

--- drag_order_extension.rb
+++ drag_order_extension.rb
@@ -3,7 +3,20 @@ class DragOrderExtension < Radiant::Extension
   description "Radiant DragOrder allows you to reorder pages funly"
   url "https://github.com/dirkkelly/radiant-drag-order"
   
-  def activate    
+  # HACK: Using define_routes instead of config/routes.rb because in Radiant
+  # Edge (~a43878e3dd05c3eaceffdb17182b63dce3a529fc) the routes from routes.rb
+  # aren't resolved. They are loaded (and show up in rake routes) but not in
+  # the right priority, i.e.
+  # ActionController::Routing::Routes.recognize_path('/admin/pages/sort.js', :method=>:put)
+  # doesn't return {:controller=>"admin/pages", :format=>"js", :action=>"sort"}
+  # like it should
+  define_routes do |map|
+    map.namespace :admin do |admin|
+      admin.resources :pages, :collection => { :sort => :put }
+    end
+  end
+  
+  def activate
     Page.send :include, DragOrder::Models::Page
     StandardTags.send :include, DragOrder::Tags::Core
     Admin::PagesController.send :include, DragOrder::Controllers::Admin::PagesController
