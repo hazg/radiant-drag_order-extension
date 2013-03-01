f5b56b3fce9e287f7943a1d3025b2013b0a32625
hazg <hazg@mail.ru>
2013-03-01 15:20:11 +0400

    dev mode SystemStackError

--- lib/drag_order/tags/core.rb
+++ lib/drag_order/tags/core.rb
@@ -9,7 +9,9 @@ module DragOrder
             options[:order].sub!(/published_at/i, 'position') unless tag.attr['by']
             options
           end
-          alias_method_chain :children_find_options, :drag_order
+          unless method_defined? :children_find_options_with_drag_order
+            alias_method_chain :children_find_options, :drag_order
+          end
         end
       end
       
